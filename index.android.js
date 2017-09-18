5e1f5011ade7bb58cf8fb67147d0b6cc34f15cd9
RageBill <kelvin@firstcodeacademy.com>
2017-09-18 18:20:08 +0800

    Finished basic pickers & adding options

--- index.android.js
+++ index.android.js
@@ -13,8 +13,10 @@ import {
   TextInput,
   SectionList,
   Button,
-  Picker
+  Picker,
+  DatePickerAndroid
 } from 'react-native';
+import Prompt from 'react-native-prompt';
 
 export default class PaymentChecker extends Component {
   constructor(props) {
@@ -31,15 +33,76 @@ export default class PaymentChecker extends Component {
         }
       ],
       text: '',
-      choice: 'Puzzle and Dragons',
-      choiceNum: 0
+      cards: [],
+      card: null,
+      cardNum: 0,
+      choices: ['Puzzle and Dragons', 'Clash Royale', 'Apple'],
+      choice: null,
+      choiceNum: 0,
+      date: null,
+      promptVisible: false,
+      picking: 'Create New Card'
+    }
+  }
+
+  componentDidMount() {
+    let sections = this.state.sections;
+    let cards = [];
+    for(let i = 0; i < sections.length; i++){
+      cards.push(sections[i].title);
+    }
+    this.setState({
+      cards: cards,
+      card: this.state.sections[0].title,
+      choice: this.state.choices[1]
+    });
+  }
+
+  pickCard = (value, index) => {
+    // Check if creating new choices
+    if(value == 'Add New Card...'){
+      this.setState({promptVisible: true, picking: 'Create New Card'});
+    } else {
+      this.setState({card: value, cardNum: index});
+    }
+  }
+
+  pickChoice = (value, index) => {
+    // Check if creatng new choices
+    if(value == 'Add New Choice...'){
+      this.setState({promptVisible: true, picking: 'Create New Choice'});
+    } else {
+      this.setState({choice: value, choiceNum: index});
     }
   }
 
   handleSubmit = () => {
+    // Getting the date
+    let date = this.state.date;
+    // If date is empty, set to today's date
+    if(!date){
+      // Getting today's date
+      let today = new Date();
+      let day = today.getDay();
+      switch(day){
+        case 0: day = 'Sun'; break;
+        case 1: day = 'Mon'; break;
+        case 2: day = 'Tue'; break;
+        case 3: day = 'Wed'; break;
+        case 4: day = 'Thu'; break;
+        case 5: day = 'Fri'; break;
+        case 6: day = 'Sat'; break;
+      }
+      // Formatting the date, e.g. 17-9-2017 (Sun)
+      date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear() + ' [' + day + ']';
+    }
+    // Getting the list sections
     let sections = this.state.sections;
-    let text = this.state.text;
-    sections[this.state.choiceNum].data.push(text);
+    // Getting the text input, prepending the date, appending the choice
+    let text = [date, '$' + this.state.text, this.state.choice].join(' ');
+    // Pushing the text input as new item
+    sections[this.state.cardNum].data.push(text);
+    // Setting the new state
     let nextState = {
       sections: sections,
       text: ''
@@ -50,65 +113,153 @@ export default class PaymentChecker extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Prompt
+          title={this.state.picking}
+          placeholder='New Entry Here'
+          visible={ this.state.promptVisible }
+          onCancel={ () => this.setState({
+            promptVisible: false
+          }) }
+          onSubmit={ (value) => {
+            if(this.state.picking == 'Create New Card'){
+              let sections = this.state.sections;
+              sections.push({title: value, data:[]});
+              let cards = [];
+              for(let i = 0; i < sections.length; i++){
+                cards.push(sections[i].title);
+              }
+              this.setState({promptVisible: false, sections: sections, cards: cards});
+            } else {
+              let choices = this.state.choices;
+              choices.push(value);
+              this.setState({promptVisible: false, choices: choices});
+            }
+          } }
+        />
         <SectionList
           sections={this.state.sections}
           renderSectionHeader={({section, index}) => <Header title={section.title}/>}
           renderItem={({item}) => <Entry text={item} key={item}/>}
         />
         <View style={styles.newInput}>
-          <Text style={{fontSize: 20, flex: 1}}>New:</Text>
+          <Text style={{fontSize: 20, flex: 1}}>$</Text>
           <TextInput
-            style={{fontSize: 18, flex: 4}}
-            placeholder='New Entry'
+            style={{fontSize: 18, flex: 10}}
+            placeholder='Price in HKD'
             value={this.state.text}
             onChangeText={(text) => this.setState({text})}
           />
           <Button
-            style={{flex: 1}}
+            style={{flex: 2}}
             onPress={this.handleSubmit}
             title='Submit'
           />
         </View>
-        <View>
-          <Picker
-            selectedValue={this.state.choice}
-            onValueChange={(value, index) => this.setState({choice: value, choiceNum: index})}
-          >
-            <Picker.Item label='Puzzle and Dragons' value='Puzzle and Dragons' />
-            <Picker.Item label='Clash Royale' value='Clash Royale' />
-          </Picker>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{flex: 1}}>
+            <CardPicker
+              cards={this.state.cards}
+              card={this.state.card}
+              cardNum={this.state.cardNum}
+              onValueChange={this.pickCard}
+            />
+          </View>
+          <View style={{flex: 1}}>
+            <ItemPicker
+              choices={this.state.choices}
+              choice={this.state.choice}
+              choiceNum={this.state.choiceNum}
+              onValueChange={this.pickChoice}
+            />
+          </View>
         </View>
       </View>
     );
   }
 }
 
+// The displayed entries component
 class Entry extends React.Component {
   render() {
     return (
-      <Text style={styles.text}>{this.props.text}</Text>
+      <Text style={styles.entry}>{this.props.text}</Text>
     )
   }
 }
 
+// The displayed headers component
 class Header extends React.Component {
   render() {
     return (
-      <Text style={styles.sectionHeader}>{this.props.title}</Text>
+      <Text style={styles.header}>{this.props.title}</Text>
+    )
+  }
+}
+
+// Card picker
+class CardPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {...props}
+  }
+
+  // Update the picker after picking
+  componentWillReceiveProps(nextProps) {
+    this.setState({...nextProps});
+  }
+
+  render() {
+    return (
+      <Picker
+        selectedValue={this.state.card}
+        onValueChange={this.props.onValueChange}
+      >
+        {this.state.cards.map((card) => {return <Picker.Item label={card} value={card}/>})}
+        <Picker.Item label='Add New Card...' value='Add New Card...'/>
+      </Picker>
+    )
+  }
+}
+
+// Payment Item Picker
+class ItemPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {...props}
+  }
+
+  // Update the picker after picker
+  componentWillReceiveProps(nextProps) {
+    this.setState({...nextProps});
+  }
+
+  render() {
+    return(
+      <Picker
+        style={{alignItems: 'center'}}
+        selectedValue={this.state.choice}
+        onValueChange={this.props.onValueChange}
+      >
+        {this.state.choices.map((item) => {return <Picker.Item label={item} value={item}/>})}
+        <Picker.Item label='Add New Choice...' value='Add New Choice...'/>
+      </Picker>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  // Outermost container
   container: {
     flex: 1,
   },
-  text: {
+  // Entries
+  entry: {
     padding: 10,
     fontSize: 18,
     height: 44,
   },
-  sectionHeader: {
+  // Headers
+  header: {
     paddingTop: 2,
     paddingLeft: 10,
     paddingRight: 10,
@@ -117,6 +268,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     backgroundColor: 'rgba(247,247,247,1.0)',
   },
+  // Input textbox
   newInput: {
     flexDirection: 'row',
     alignItems: 'center'
